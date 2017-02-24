71daeacd2cb91ecad33e6c698312306d95bc5b60
Marian Ben <marik1995ben@mail.ru>
2017-02-24 17:26:30 +0000

    new3

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function square_equation() {
 
   var d = b * b - 4 * a * c;
   if ( d < 0 ) {
-    string = "пара комплексно-сопряженных корней<br>x<sub>1</sub> = (";
+    string = "пара комплексно спряжених чисел<br>x<sub>1</sub> = (";
     string += - b / ( 2 * a );
     string += ", ";
     string += Math.sqrt( -d ) / ( 2 * a );
@@ -17,11 +17,11 @@ function square_equation() {
     string += ").";
   } else {
  if ( d == 0 ) {
-        string = "два одинаковых вещественных корня:<br>x<sub>1</sub> = x<sub>2</sub> = ";
+        string = "два однакових кореня:<br>x<sub>1</sub> = x<sub>2</sub> = ";
         string += -b / ( 2 * a );
         string += ".";
       } else {
-        string = "два различных вещественных корня:<br>x<sub>1</sub> = ";
+        string = "два різних кореня:<br>x<sub>1</sub> = ";
         string += -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
         string += " x<sub>2</sub> = ";
         string += -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
